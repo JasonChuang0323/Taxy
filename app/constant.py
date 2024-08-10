33cd871956b75c42816075c0ee76622a749c1429
Jason <jason@JasondeAir.lan>
2024-08-10 17:51:03 +1000

    Added user api/model

---
+++ app/constant.py
@@ -0,0 +1,7 @@
+from app.model.user import User
+
+## Fake data
+user1 = User("1", "Jas1", "mq Block A")
+user2 = User("2", "Jas2", "mq Block B")
+user3 = User("3", "jas3", "mq Block C")
+UserList = [user1, user2, user3]
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, jsonify, request
+from .constant import UserList
 
 # Create a Blueprint for the routes
 routes_bp = Blueprint('routes', __name__)
+
 
 @routes_bp.route('/')
 def home():
@@ -12,3 +14,8 @@ def greet():
     data = request.get_json()
     name = data.get('name', 'Stranger')
     return jsonify({'message': f'Hello, {name}!'})
+
+@routes_bp.route('/api/list_user')
+def list_user():
+    user_list = list(user.to_dict() for user in UserList)
+    return jsonify(user_list)
